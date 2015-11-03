e6acc57e75c2cd3f1fd2e865faefeed04149d53d
Tanner Evins <tanner@tannerevins.com>
2015-11-03 14:36:28 -0600

    Encapsulate common shader program in class

--- VisualPractice/shader.cpp
+++ VisualPractice/shader.cpp
@@ -1,4 +1,8 @@
 #include "shader.h"
+#include "mesh.h"
+#include "texture.h"
+
+#include <glm/gtc/type_ptr.hpp>
 
 #include <stdexcept>
 #include <fstream>
@@ -125,5 +129,72 @@ namespace te
         }
 
         return program;
+    }
+
+    Shader::Shader(const glm::mat4& projection, const glm::mat4& model)
+        : mProgram(loadProgram("tiled_map.glvs", "tiled_map.glfs"))
+        , mLastView()
+    {
+        glUseProgram(mProgram);
+
+        GLint projectionMatrixLocation = glGetUniformLocation(mProgram, "te_ProjectionMatrix");
+        if (projectionMatrixLocation == -1) { throw std::runtime_error("te_ProjectionMatrix: not a valid program variable."); }
+        glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
+
+        GLint modelMatrixLocation = glGetUniformLocation(mProgram, "te_ModelMatrix");
+        if (modelMatrixLocation == -1) { throw std::runtime_error("te_ModelMatrix: not a valid program variable."); }
+        glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+        GLint viewMatrixLocation = glGetUniformLocation(mProgram, "te_ViewMatrix");
+        if (viewMatrixLocation == -1) { throw std::runtime_error{ "te_ViewMatrix: not a valid program variable." }; }
+        glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(glm::mat4{}));
+    }
+
+    void Shader::destroy()
+    {
+        glDeleteProgram(mProgram);
+    }
+
+    Shader::~Shader()
+    {
+        destroy();
+    }
+
+    Shader::Shader(Shader&& o)
+        : mProgram(o.mProgram)
+        , mLastView(o.mLastView)
+    {
+        o.mProgram = 0;
+    }
+
+    Shader& Shader::operator=(Shader&& o)
+    {
+        destroy();
+
+        mProgram = o.mProgram;
+        mLastView = o.mLastView;
+        o.mProgram = 0;
+
+        return *this;
+    }
+
+    void Shader::draw(const glm::mat4& modelview, const Mesh& mesh)
+    {
+        glUseProgram(mProgram);
+
+        if (modelview != mLastView) {
+            GLint viewMatrixLocation = glGetUniformLocation(mProgram, "te_ViewMatrix");
+            if (viewMatrixLocation == -1) { throw std::runtime_error{ "te_ViewMatrix: not a valid program variable." }; }
+            glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(modelview));
+            mLastView = modelview;
+        }
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, mesh.getTexture(0)->getID());
+        glBindVertexArray(mesh.getVAO());
+        glDrawElements(GL_TRIANGLES, mesh.getElementCount(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glUseProgram(0);
     }
 }
--- VisualPractice/shader.h
+++ VisualPractice/shader.h
@@ -2,14 +2,35 @@
 #define TE_SHADER_H
 
 #include "gl.h"
+#include <glm/glm.hpp>
 #include <string>
 
 namespace te
 {
+    class Mesh;
+
     std::string getShaderLog(GLuint shader);
     std::string getProgramLog(GLuint program);
     GLuint loadShader(const std::string& path, GLenum shaderType);
     GLuint loadProgram(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+
+    class Shader {
+    public:
+        Shader(const glm::mat4& projection, const glm::mat4& model);
+        ~Shader();
+        Shader(Shader&&);
+        Shader& operator=(Shader&&);
+
+        void draw(const glm::mat4& modelview, const Mesh&);
+    private:
+        GLuint mProgram;
+        glm::mat4 mLastView;
+
+        void destroy();
+
+        Shader(const Shader&) = delete;
+        Shader& operator=(const Shader&) = delete;
+    };
 }
 
 #endif
