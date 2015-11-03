84949e8a26f576cd78153316532c5c150d0f4069
Tanner Evins <tanner@tannerevins.com>
2015-11-03 14:40:28 -0600

    Simplify AnimationComponent
    Handle animation construction with AnimationFactory. Pass constructed
    animations to AnimationComponent to associate with entities.

--- VisualPractice/animation_component.cpp
+++ VisualPractice/animation_component.cpp
@@ -6,50 +6,70 @@ namespace te
     AnimationComponent::AnimationComponent(size_t capacity)
         : Component(capacity) {}
 
-    void AnimationComponent::setAnimations(
-        const Entity& entity,
-        const TMX& tmx,
-        const TMX::Tileset::Tile::ObjectGroup::Object& object,
-        MeshManager& meshManager)
+    void AnimationComponent::setAnimations(const Entity& entity, const std::vector<Frame>& frames)
     {
-        AnimationInstance instance;
-
-        unsigned tilesetIndex = getTilesetIndex(tmx, object.gid);
-        const TMX::Tileset& tileset = tmx.tilesets.at(tilesetIndex);
-
-        const TMX::Tileset::Tile* pObjectTile = nullptr;
-        std::for_each(
-            std::begin(tileset.tiles),
-            std::end(tileset.tiles),
-            [&pObjectTile, &object, &tileset](const TMX::Tileset::Tile& tile) {
-
-            if (tile.id == object.gid - tileset.firstgid && tile.animation.size() > 0) {
-                pObjectTile = &tile;
-            }
-        });
-
-        if (pObjectTile) {
-            std::for_each(
-                std::begin(pObjectTile->animation),
-                std::end(pObjectTile->animation),
-                [&](const TMX::Tileset::Tile::Frame& frame) {
-
-                instance.frames.push_back(AnimationInstance::Frame{
-                    meshManager[frame.tileid + tileset.firstgid],
-                    frame.duration
-                });
-            });
-        } else {
-            instance.frames.push_back(AnimationInstance::Frame{
-                meshManager[object.gid],
-                0
-            });
-        }
-
+        if (frames.size() == 0) { throw std::runtime_error{ "AnimationComponent::setAnimations: must have at least one frame." }; }
         if (!hasInstance(entity)) {
-            createInstance(entity, std::move(instance));
+            createInstance(entity, { frames, 0, 0 });
         } else {
-            at(entity) = std::move(instance);
+            at(entity) = { frames, 0, 0 };
         }
     }
+
+    void AnimationComponent::setAnimations(const Entity& entity, std::vector<Frame>&& frames)
+    {
+        if (frames.size() == 0) { throw std::runtime_error{ "AnimationComponent::setAnimations: must have at least one frame." }; }
+        if (!hasInstance(entity)) {
+            createInstance(entity, { std::move(frames), 0, 0 });
+        } else {
+            at(entity) = { std::move(frames), 0, 0 };
+        }
+    }
+
+    //void AnimationComponent::setAnimations(
+    //    const Entity& entity,
+    //    const TMX& tmx,
+    //    const TMX::Tileset::Tile::ObjectGroup::Object& object,
+    //    MeshManager& meshManager)
+    //{
+    //    AnimationInstance instance;
+
+    //    unsigned tilesetIndex = getTilesetIndex(tmx, object.gid);
+    //    const TMX::Tileset& tileset = tmx.tilesets.at(tilesetIndex);
+
+    //    const TMX::Tileset::Tile* pObjectTile = nullptr;
+    //    std::for_each(
+    //        std::begin(tileset.tiles),
+    //        std::end(tileset.tiles),
+    //        [&pObjectTile, &object, &tileset](const TMX::Tileset::Tile& tile) {
+
+    //        if (tile.id == object.gid - tileset.firstgid && tile.animation.size() > 0) {
+    //            pObjectTile = &tile;
+    //        }
+    //    });
+
+    //    if (pObjectTile) {
+    //        std::for_each(
+    //            std::begin(pObjectTile->animation),
+    //            std::end(pObjectTile->animation),
+    //            [&](const TMX::Tileset::Tile::Frame& frame) {
+
+    //            instance.frames.push_back(AnimationInstance::Frame{
+    //                meshManager[frame.tileid + tileset.firstgid],
+    //                frame.duration
+    //            });
+    //        });
+    //    } else {
+    //        instance.frames.push_back(AnimationInstance::Frame{
+    //            meshManager[object.gid],
+    //            0
+    //        });
+    //    }
+
+    //    if (!hasInstance(entity)) {
+    //        createInstance(entity, std::move(instance));
+    //    } else {
+    //        at(entity) = std::move(instance);
+    //    }
+    //}
 }
--- VisualPractice/animation_component.h
+++ VisualPractice/animation_component.h
@@ -4,36 +4,39 @@
 #include "component.h"
 #include "mesh.h"
 #include "tmx.h"
+#include "animation_factory.h"
 
 #include <vector>
 #include <memory>
 
 namespace te
 {
-    class MeshManager;
-
     struct AnimationInstance {
-        struct Frame {
-            std::shared_ptr<Mesh> mesh;
-            unsigned duration;
-        };
         std::vector<Frame> frames;
+        float currDuration;
+        unsigned currFrameIndex;
     };
 
     class AnimationComponent : public Component<AnimationInstance> {
     public:
         AnimationComponent(size_t capacity = 1024);
 
-        void setAnimations(
-            const Entity& entity,
-            const TMX& tmx,
-            const TMX::Tileset::Tile::ObjectGroup::Object& object,
-            MeshManager& meshManager);
+        void setAnimations(const Entity& entity, const std::vector<Frame>& frames);
+        void setAnimations(const Entity& entity, std::vector<Frame>&& frames);
+        //void setAnimations(
+        //    const Entity& entity,
+        //    const TMX& tmx,
+        //    const TMX::Tileset::Tile::ObjectGroup::Object& object,
+        //    MeshManager& meshManager);
 
     private:
+        friend class RenderSystem;
+
         AnimationComponent(const AnimationComponent&) = delete;
         AnimationComponent& operator=(const AnimationComponent&) = delete;
     };
+
+    typedef std::shared_ptr<AnimationComponent> AnimationPtr;
 }
 
 #endif
