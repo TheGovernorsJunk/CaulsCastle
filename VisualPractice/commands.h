b4c0e72db659adff77f16277cf8634d6efc32a51
Tanner Evins <tanner@tannerevins.com>
2015-11-06 12:11:21 -0600

    Comment out old Command to avoid name collisions

--- VisualPractice/commands.h
+++ VisualPractice/commands.h
@@ -13,15 +13,15 @@ namespace te
 
     enum class Action { UP, DOWN };
 
-    typedef std::function<void()> Command;
+    //typedef std::function<void()> Command;
 
-    typedef std::map<Action, Command> CommandMap;
+    //typedef std::map<Action, Command> CommandMap;
 
-    CommandMap createPaddleCommandMap(std::shared_ptr<Rectangle> pPaddle);
+    //CommandMap createPaddleCommandMap(std::shared_ptr<Rectangle> pPaddle);
 
-    typedef std::map<std::pair<SDL_Keycode, Uint32>, Action> KeyMap;
+    //typedef std::map<std::pair<SDL_Keycode, Uint32>, Action> KeyMap;
 
-    KeyMap createPaddleKeyMap(unsigned int configN = 1);
+    //KeyMap createPaddleKeyMap(unsigned int configN = 1);
 }
 
 #endif /* TE_COMMANDS_H */
