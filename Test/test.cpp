a3dfc07b160abfd90cdae8eff101b2b88ffd63d5
Tanner Evins <tanner@tannerevins.com>
2015-11-09 17:44:04 -0600

    Include VisualPractice as Test dependency

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,6 +1,11 @@
+#include <entity_manager.h>
+
 #include <gtest/gtest.h>
 
-TEST(test, name)
-{
-    EXPECT_EQ(0, 0);
+TEST(EntityManager, basic) {
+    te::EntityManager em;
+    te::Entity entity = em.create();
+    EXPECT_EQ(true, em.isAlive(entity));
+    em.destroy(entity);
+    EXPECT_EQ(false, em.isAlive(entity));
 }
