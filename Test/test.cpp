058d47f212b766617ae5d6e09cb964370642c812
Tanner Evins <tanner@tannerevins.com>
2015-11-10 04:32:43 -0600

    Add commented out fixture

--- Test/test.cpp
+++ Test/test.cpp
@@ -12,11 +12,20 @@ namespace te
 
     ::testing::Environment* const env = ::testing::AddGlobalTestEnvironment(new Env());
 
-    TEST(EntityManager, basic) {
+    TEST(EntityManager, Basic) {
         te::EntityManager em;
         te::Entity entity = em.create();
         EXPECT_EQ(true, em.isAlive(entity));
         em.destroy(entity);
         EXPECT_EQ(false, em.isAlive(entity));
     }
+
+    //class WindowTest : public ::testing::Test {
+    //public:
+    //    WindowPtr pWindow;
+    //    WindowTest() : pWindow(createWindowOpenGL("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 32, 32, SDL_WINDOW_SHOWN)) {}
+    //};
+
+    //TEST_F(WindowTest, Basic) {
+    //}
 }
