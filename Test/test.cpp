861deaec572f3cf92a10e4f0e7a9f8e67ec6a870
Tanner Evins <tanner@tannerevins.com>
2015-11-09 18:56:58 -0600

    Link libraries, add initialization

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,11 +1,22 @@
+#include <wrappers.h>
 #include <entity_manager.h>
 
 #include <gtest/gtest.h>
 
-TEST(EntityManager, basic) {
-    te::EntityManager em;
-    te::Entity entity = em.create();
-    EXPECT_EQ(true, em.isAlive(entity));
-    em.destroy(entity);
-    EXPECT_EQ(false, em.isAlive(entity));
+namespace te
+{
+    class Env : public ::testing::Environment {
+    private:
+        Initialization init;
+    };
+
+    ::testing::Environment* const env = ::testing::AddGlobalTestEnvironment(new Env());
+
+    TEST(EntityManager, basic) {
+        te::EntityManager em;
+        te::Entity entity = em.create();
+        EXPECT_EQ(true, em.isAlive(entity));
+        em.destroy(entity);
+        EXPECT_EQ(false, em.isAlive(entity));
+    }
 }
