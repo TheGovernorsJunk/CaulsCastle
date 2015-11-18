98716e59deb46b3caf050c245370157964e01bff
Tanner Evins <tanner@tannerevins.com>
2015-11-18 12:22:19 -0600

    Add MapRunner project to load maps by argument

---
+++ src/MapRunner/main.cpp
@@ -0,0 +1,25 @@
+#include <wrappers.h>
+#include <tmx.h>
+
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+    const std::size_t BUFFER_SIZE = 256;
+
+    if (argc != 2) {
+        std::cerr << "Incorrect usage: Must supply path to Tiled export Lua file." << std::endl;
+        return -1;
+    }
+
+    try {
+        te::Initialization init;
+        te::TMX tmx(argv[1]);
+    } catch (std::exception ex) {
+        std::cerr << ex.what() << std::endl;
+        return -1;
+    }
+    return 0;
+}
