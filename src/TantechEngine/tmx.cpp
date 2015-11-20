8a34e91873793fc9a14ff1f75197e666a7f2d8a6
Tanner Evins <tanner@tannerevins.com>
2015-11-20 15:50:41 -0600

    Support per-object scaling

--- src/TantechEngine/tmx.cpp
+++ src/TantechEngine/tmx.cpp
@@ -379,7 +379,9 @@ namespace te
                 transformComponent.setLocalTransform(
                     entity,
                     // Subtract by height to compensate for TMX odd positions
-                    model * glm::translate(glm::vec3(object.x, object.y - object.height, layerIndex)));
+                    model * glm::scale(
+                        glm::translate(glm::vec3(object.x, object.y - object.height, layerIndex)),
+                        glm::vec3((float)object.width / (float)tileset.tilewidth, (float)object.height / (float)tileset.tileheight, 1.f)));
 
                 std::shared_ptr<const Animation> pAnimation(new Animation{
                     animationFactory.create(object.gid)
