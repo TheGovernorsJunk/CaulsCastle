918dfa977a485b035316c6a4344eca4425e5a52c
Tanner Evins <tanner@tannerevins.com>
2015-12-02 13:09:51 -0600

    Add hero spritesheet, closes #22

---
+++ assets/spritesheets/hero/hero.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hero" tilewidth="64" tileheight="64" tilecount="6">
+ <image source="hero.png" width="128" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/spritesheets/hero/info.lua
@@ -0,0 +1,8 @@
+-- Copyright (c) Ryan Conley 2015
+hero = {
+  tilewidth = 64
+  tileheight = 64
+  spacing = 0
+  margin = 0
+  artist = "Ryan Conley"
+}
