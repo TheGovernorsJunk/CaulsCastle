c789c2a8e4230196a012c0fef159000a5a7fcced
Tanner Evins <tanner@tannerevins.com>
2015-12-21 10:38:42 -0600

    Add run animation and transitions

--- unity/MetroidClone/Assets/Player.cs
+++ unity/MetroidClone/Assets/Player.cs
@@ -4,24 +4,27 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	public float maxSpeed = 100f;
+	public float jumpDistance = 5f;
 
 	private Rigidbody2D rb2D;
+	private Animator anim;
 
 	void Awake()
 	{
 		rb2D = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 
 	void FixedUpdate ()
 	{
 		int horizontal = (int)Input.GetAxisRaw("Horizontal");
-		rb2D.velocity -= new Vector2(rb2D.velocity.x, 0);
-		rb2D.velocity += new Vector2(horizontal * maxSpeed, 0);
+		rb2D.velocity = new Vector2(horizontal * maxSpeed, rb2D.velocity.y);
+		anim.SetFloat("speed", Mathf.Abs(horizontal));
 
 		int vertical = (int)Input.GetAxisRaw("Vertical");
 		if (vertical == 1 && OnGround)
 		{
-			rb2D.velocity += new Vector2(0, 5);
+			rb2D.velocity += new Vector2(0, jumpDistance);
 		}
 	}
 
