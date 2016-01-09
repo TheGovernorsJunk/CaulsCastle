77b3aa5e7a0983a889392e8181f2fe396acfb834
Tanner Evins <tanner@tannerevins.com>
2016-01-09 12:08:10 -0600

    Add camera movement script

---
+++ unity/MetroidClone/Assets/Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject subject;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 subPosition = subject.transform.position;
+		subPosition.z = transform.position.z;
+		transform.position = subPosition;
+	}
+}
