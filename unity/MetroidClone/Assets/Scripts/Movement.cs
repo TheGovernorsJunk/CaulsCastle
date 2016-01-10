75f5fd9735b55e62a0496421fbf9368bd30c96de
Tanner Evins <tanner@tannerevins.com>
2016-01-10 12:44:58 -0600

    Add Movement script to Elisa

---
+++ unity/MetroidClone/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float maxSpeed = 5f;
+	public Rigidbody2D rigidbody;
+
+	void FixedUpdate ()
+	{
+		int horizontal = (int)Input.GetAxisRaw("Horizontal");
+		rigidbody.velocity = new Vector2(
+			horizontal * maxSpeed * Time.deltaTime,
+			rigidbody.velocity.y);
+	}
+}
