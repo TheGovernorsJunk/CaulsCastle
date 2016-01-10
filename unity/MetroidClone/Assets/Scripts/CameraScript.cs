e0c15294a44a80ffa90295446e1498d5494964ad
Tanner Evins <tanner@tannerevins.com>
2016-01-10 13:50:21 -0600

    Begin parallax effect script

--- unity/MetroidClone/Assets/Scripts/CameraScript.cs
+++ unity/MetroidClone/Assets/Scripts/CameraScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Camera : MonoBehaviour {
+public class CameraScript : MonoBehaviour {
 
 	public GameObject subject;
 	
---
+++ unity/MetroidClone/Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+	public float parallaxFactorX = 1f;
+	public float parallaxFactorY = 1f;
+	public SpriteRenderer parallaxBackground;
+
+	private Camera camera;
+
+	void Start()
+	{
+		camera = GetComponent<Camera>();
+	}
+
+	void Update ()
+	{
+		Vector3 position = new Vector3(
+			parallaxFactorX * camera.transform.position.x,
+			parallaxFactorY * camera.transform.position.y);
+		position.z = parallaxBackground.transform.position.z;
+		parallaxBackground.transform.position = position;
+	}
+}
