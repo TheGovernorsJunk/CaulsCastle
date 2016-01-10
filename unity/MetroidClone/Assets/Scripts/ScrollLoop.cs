7df116fc6206cc5c1777061bb6d934dd58cf245f
Tanner Evins <tanner@tannerevins.com>
2016-01-10 16:06:22 -0600

    Add scrolling background effect

---
+++ unity/MetroidClone/Assets/Scripts/ScrollLoop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollLoop : MonoBehaviour
+{
+	public int moveUnits = 1;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		SpriteRenderer renderer = other.GetComponent<SpriteRenderer>();
+		other.transform.Translate(new Vector3(
+			moveUnits * renderer.bounds.size.x, 0, 0));
+	}
+}
