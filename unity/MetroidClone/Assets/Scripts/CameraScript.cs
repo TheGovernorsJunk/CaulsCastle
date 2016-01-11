0d033e377ba77e2c380385f935215e2c6f14ed69
Tanner Evins <tanner@tannerevins.com>
2016-01-10 19:52:29 -0600

    Add offset parameter to camera script

--- unity/MetroidClone/Assets/Scripts/CameraScript.cs
+++ unity/MetroidClone/Assets/Scripts/CameraScript.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
 	public GameObject subject;
+	public Vector2 offset = new Vector2(0, 0);
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Vector3 subPosition = subject.transform.position;
-		subPosition.z = transform.position.z;
+		Vector3 subPosition = new Vector3(
+			subject.transform.position.x + offset.x,
+			subject.transform.position.y + offset.y,
+			transform.position.z);
 		transform.position = subPosition;
 	}
 }
