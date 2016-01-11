d5201e6cf9d77504b4f833fb0fae02dc61294850
Tanner Evins <tanner@tannerevins.com>
2016-01-11 09:37:40 -0600

    Add hit box to slice animation

---
+++ unity/MetroidClone/Assets/Scripts/Slice.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slice : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D target)
+	{
+		Debug.Log("Hit!");
+	}
+
+	void OnMove(int direction)
+	{
+		if (direction == 1)
+		{
+			transform.localScale = new Vector3(1,1,1);
+		}
+		else if (direction == -1)
+		{
+			transform.localScale = new Vector3(-1,1,1);
+		}
+	}
+}
