bd39c8fb1d41dddeb55ec3a0faf191589f56ca60
Tanner Evins <tanner@tannerevins.com>
2016-03-01 14:40:05 -0600

    Attach damage animation to controller

---
+++ unity/MetroidClone/Assets/Scripts/DamageAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageAnimation : MonoBehaviour
+{
+	Animator animator;
+	int hitHash = Animator.StringToHash("hit");
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D attacker)
+	{
+		if (attacker.tag == "HitBox")
+		{
+			animator.SetTrigger(hitHash);
+		}
+	}
+}
