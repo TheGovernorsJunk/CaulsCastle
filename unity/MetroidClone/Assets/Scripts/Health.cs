ccac526187088ed54acda8c1410d3ce5f9a6ffe4
Tanner Evins <tanner@tannerevins.com>
2016-03-09 16:12:57 -0600

    Add health bar to HUD

--- unity/MetroidClone/Assets/Scripts/Health.cs
+++ unity/MetroidClone/Assets/Scripts/Health.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Health : MonoBehaviour
@@ -8,6 +9,8 @@ public class Health : MonoBehaviour
 		get { return hp; }
 	}
 
+	public Slider healthSlider;
+
 	private Stats stats;
 	private int maxHP;
 	private int hp;
@@ -16,13 +19,21 @@ public class Health : MonoBehaviour
 	{
 		stats = GetComponent<Stats>();
 		maxHP = hp = stats.vitality * 10;
+		UpdateSlider();
 	}
-	
+
+	void UpdateSlider()
+	{
+		if (healthSlider)
+			healthSlider.value = (float)hp / (float)maxHP;
+	}
+
 	void OnHit(DamageVector damageVector)
 	{
 		int damage = (int)(damageVector.physicalDamage - (stats.resistance * 0.2));
 		BroadcastMessage("OnDamageTaken", damage);
 		hp -= damage;
+		UpdateSlider();
 		if (hp <= 0)
 			Destroy(gameObject);
 	}
