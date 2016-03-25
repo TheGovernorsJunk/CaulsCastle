4c20c6148bbf4ff1346b040a257b3c21ad86e0cf
Tanner Evins <tanner@tannerevins.com>
2016-03-25 13:28:12 -0500

    Add more vector operations

--- Zelda/vector_ops.cpp
+++ Zelda/vector_ops.cpp
@@ -12,10 +12,32 @@ namespace te
 		return v / std::sqrtf(v.x * v.x + v.y * v.y);
 	}
 
+	float lengthSq(const sf::Vector2f& v)
+	{
+		return v.x * v.x + v.y * v.y;
+	}
+
 	float distanceSq(const sf::Vector2f& a, const sf::Vector2f& b)
 	{
 		float x = a.x - b.x;
 		float y = a.y - b.y;
 		return x * x + y * y;
+	}
+
+	sf::Vector2f truncate(const sf::Vector2f& v, float maxMagnitude)
+	{
+		if (lengthSq(v) > (maxMagnitude * maxMagnitude))
+		{
+			return normalize(v) * maxMagnitude;
+		}
+		else
+		{
+			return v;
+		}
+	}
+
+	sf::Vector2f perp(const sf::Vector2f& v)
+	{
+		return sf::Vector2f(-v.y, v.x);
 	}
 }
--- Zelda/vector_ops.h
+++ Zelda/vector_ops.h
@@ -7,7 +7,10 @@ namespace te
 {
 	float length(const sf::Vector2f& v);
 	sf::Vector2f normalize(const sf::Vector2f& v);
+	float lengthSq(const sf::Vector2f& v);
 	float distanceSq(const sf::Vector2f& a, const sf::Vector2f& b);
+	sf::Vector2f truncate(const sf::Vector2f& v, float maxMagnitude);
+	sf::Vector2f perp(const sf::Vector2f& v);
 }
 
 #endif
