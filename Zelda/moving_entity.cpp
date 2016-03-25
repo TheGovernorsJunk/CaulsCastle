8ea6bb5a4fc0d1f52d8721b9fa374b56f9a76cb0
Tanner Evins <tanner@tannerevins.com>
2016-03-25 13:27:43 -0500

    Add updateOnForce method to MovingEntity

--- Zelda/moving_entity.cpp
+++ Zelda/moving_entity.cpp
@@ -1,15 +1,44 @@
 #include "moving_entity.h"
+#include "vector_ops.h"
 
 namespace te
 {
-	MovingEntity::MovingEntity()
-		: BaseGameEntity() {}
+	MovingEntity::MovingEntity(float maxSpeed, float maxForce, float maxTurnRate)
+		: BaseGameEntity()
+		, mMaxSpeed(maxSpeed)
+		, mMaxForce(maxForce)
+		, mMaxTurnRate(maxTurnRate)
+		, mVelocity(0.f, 0.f)
+		, mHeading(0.f, -1.f)
+		, mSide(1.f, 0.f)
+		, mMass(1.f)
+	{}
 
 	MovingEntity::~MovingEntity() {}
+
+	void MovingEntity::updateOnForce(const sf::Time& dt, sf::Vector2f steeringForce)
+	{
+		float seconds = dt.asSeconds();
+		sf::Vector2f acceleration = steeringForce / mMass;
+		mVelocity += acceleration * seconds;
+		mVelocity = truncate(mVelocity, mMaxSpeed);
+		move(mVelocity * seconds);
+
+		if (lengthSq(mVelocity) > 0.00000001f)
+		{
+			mHeading = normalize(mVelocity);
+			mSide = perp(mHeading);
+		}
+	}
 
 	float MovingEntity::getMaxSpeed() const
 	{
 		return mMaxSpeed;
+	}
+
+	float MovingEntity::getMaxForce() const
+	{
+		return mMaxForce;
 	}
 
 	sf::Vector2f MovingEntity::getVelocity() const
--- Zelda/moving_entity.h
+++ Zelda/moving_entity.h
@@ -8,14 +8,23 @@ namespace te
 	class MovingEntity : public BaseGameEntity
 	{
 	public:
-		MovingEntity();
+		MovingEntity(float maxSpeed = 0.f, float maxForce = 0.f, float maxTurnRate = 0.f);
 		virtual ~MovingEntity();
 
 		float getMaxSpeed() const;
+		float getMaxForce() const;
 		sf::Vector2f getVelocity() const;
+
+		void updateOnForce(const sf::Time& dt, sf::Vector2f steeringForce);
+
 	private:
 		float mMaxSpeed;
+		float mMaxForce;
+		float mMaxTurnRate;
 		sf::Vector2f mVelocity;
+		sf::Vector2f mHeading;
+		sf::Vector2f mSide;
+		float mMass;
 	};
 }
 
