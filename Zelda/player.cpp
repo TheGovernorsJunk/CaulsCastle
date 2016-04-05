20257d9fd768d65f12b76202189f7bf26d1e4271
Tanner Evins <tanner@tannerevins.com>
2016-04-05 15:50:10 -0500

    Correct Player collider

--- Zelda/player.cpp
+++ Zelda/player.cpp
@@ -16,7 +16,6 @@ namespace te
 		, mpFixture(nullptr)
 	{
 		assert(playerObject.name == "Player");
-		setOrigin(transform.transformPoint({ playerObject.width / 2.f, playerObject.height / 2.f }));
 
 		sf::Vector2f radiusVector = transform.transformPoint({ playerObject.width / 2.f, playerObject.height / 2.f });
 		mRadius = std::max(radiusVector.x, radiusVector.y);
@@ -63,12 +62,14 @@ namespace te
 	{
 		states.transform *= getTransform();
 		sf::CircleShape shape(mRadius);
+		shape.setOrigin(mRadius, mRadius);
 		shape.setFillColor(sf::Color::Blue);
 		target.draw(shape, states);
 
 		// Draw collider
 		b2AABB aabb = mpFixture->GetAABB(0);
 		sf::RectangleShape rectShape(sf::Vector2f(aabb.GetExtents().x * 2, aabb.GetExtents().y * 2));
+		rectShape.setOrigin(rectShape.getSize().x / 2.f, rectShape.getSize().y / 2.f);
 		sf::Color color = sf::Color::Red;
 		color.a = 128;
 		rectShape.setFillColor(color);
