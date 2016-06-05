cac092ac0aaf3a609c8879eaf1c20cd470110a64
Tanner Evins <tanner@tannerevins.com>
2016-06-04 22:26:12 -0500

    Modify `pack.sh' to be called from anywhere

--- assets/scripts/pack.sh
+++ assets/scripts/pack.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-TexturePacker --trim-mode None --format xml --data ../spritesheets/hero/hero.xml --sheet ../spritesheets/hero/hero.png ../images/hero/clips
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TexturePacker --trim-mode None --format xml --data "$DIR"/../spritesheets/hero/hero.xml --sheet "$DIR"/../spritesheets/hero/hero.png "$DIR"/../images/hero/clips
 
