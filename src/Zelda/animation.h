46d9d23666339c8a9e3b6535aeb8b30edd0444e6
Tanner Evins <tanner@tannerevins.com>
2016-06-06 09:58:59 -0500

    Add preliminary load method to Animation
    Construct Animation objects from TextureAtlas data.

--- src/Zelda/animation.h
+++ src/Zelda/animation.h
@@ -2,12 +2,15 @@
 #define TE_ANIMATION_H
 
 #include "typedefs.h"
+#include "texture_atlas.h"
 
 #include <SFML/Graphics.hpp>
 
 #include <string>
 #include <vector>
 #include <list>
+#include <iterator>
+#include <algorithm>
 
 namespace te
 {
@@ -17,6 +20,25 @@ namespace te
 	{
 	public:
 		static std::vector<Animation> load(const std::string& filename, TextureManager& textureManager);
+
+		template <typename Iter>
+		static void load(const TextureAtlas& atlas, TextureManager& textureManager, Iter out)
+		{
+			std::vector<TextureAtlas::Animation> animationData;
+			atlas.getAnimations(std::back_inserter(animationData));
+			std::transform(animationData.begin(), animationData.end(), out, [&textureManager](auto& anim) {
+				std::vector<Clip> clips;
+				for (auto& clip : anim.clips)
+				{
+					int index = clip.index;
+					TextureID spriteID = clip.spriteID;
+					sf::Sprite sprite = textureManager.getSprite(spriteID);
+					clips.push_back({spriteID, sprite});
+				}
+
+				return Animation{anim.id, 83, std::move(clips)};
+			});
+		}
 
 		TextureID getID() const;
 		int getMillisecondsPerClip() const;
