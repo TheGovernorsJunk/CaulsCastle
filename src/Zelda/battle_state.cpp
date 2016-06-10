d8ba80d0098c69a961a0592d93ad3a33a249d748
Tanner Evins <tanner@tannerevins.com>
2016-06-10 04:16:42 -0500

    Test walk right animation

--- src/Zelda/battle_state.cpp
+++ src/Zelda/battle_state.cpp
@@ -19,7 +19,7 @@ namespace te
 	{
 		void enter(Fighter& entity, StateMachine<Fighter>&)
 		{
-			entity.getAnimator().setAnimation(TextureManager::getID("PriestWalkDown"));
+			entity.getAnimator().setAnimation(TextureManager::getID("PriestWalkRight"));
 		}
 
 		void execute(Fighter& entity, StateMachine<Fighter>&, const sf::Time& dt) {}
