dc592dbb738fe41b2a773fce70bc6534177561d4
Tanner Evins <tanner@tannerevins.com>
2016-06-13 10:15:48 -0500

    Add function to create isometric background

--- src/Zelda/utilities.cpp
+++ src/Zelda/utilities.cpp
@@ -1,6 +1,7 @@
 #include "utilities.h"
 
 #include <algorithm>
+#include <array>
 
 namespace te
 {
@@ -8,5 +9,27 @@ namespace te
 	{
 		auto result = std::find_end(filename.begin(), filename.end(), delimiter.begin(), delimiter.end());
 		return result != filename.end() ? std::string(filename.begin(), result + 1) : "";
+	}
+
+	sf::Texture makeIsometricPlaceholder(int scale)
+	{
+		sf::RenderTexture texture;
+		texture.create(32 * scale, 16 * scale);
+		texture.setRepeated(true);
+
+		std::array<sf::Vertex, 8> lines = {
+			sf::Vertex(sf::Vector2f(16.f, 0)), sf::Vertex(sf::Vector2f(33.f, 8.f)),
+			sf::Vertex(sf::Vector2f(33.f, 8.f)), sf::Vertex(sf::Vector2f(16.f, 16.f)),
+			sf::Vertex(sf::Vector2f(17.f, 16.f)), sf::Vertex(sf::Vector2f(0, 8.f)),
+			sf::Vertex(sf::Vector2f(0, 8.f)), sf::Vertex(sf::Vector2f(17.f, 0))
+		};
+		std::for_each(lines.begin(), lines.end(), [scale](sf::Vertex& v) {
+			v.position = v.position * (float)scale;
+		});
+		texture.draw(lines.data(), 8, sf::Lines);
+
+		texture.display();
+
+		return texture.getTexture();
 	}
 }
--- src/Zelda/utilities.h
+++ src/Zelda/utilities.h
@@ -1,11 +1,15 @@
 #ifndef TE_UTILITIES_H
 #define TE_UTILITIES_H
 
+#include <SFML/Graphics.hpp>
+
 #include <string>
 
 namespace te
 {
 	std::string getDir(const std::string& filename, const std::string& delimiter = "/");
+
+	sf::Texture makeIsometricPlaceholder(int scale = 1);
 }
 
 #endif
