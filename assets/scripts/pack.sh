58c75c29f25dbaad5d51f4149f86cdbf0bbebd37
Tanner Evins <tanner@tannerevins.com>
2016-06-19 08:47:58 -0500

    Add tps file, adjust pack script, execute

--- assets/scripts/pack.sh
+++ assets/scripts/pack.sh
@@ -2,5 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TexturePacker --trim-mode None --format xml --data "$DIR"/../spritesheets/hero/hero.xml --sheet "$DIR"/../spritesheets/hero/hero.png "$DIR"/../images/hero/clips
-TexturePacker --trim-mode None --format xml --data "$DIR"/../spritesheets/priest/priest.xml --sheet "$DIR"/../spritesheets/priest/priest.png "$DIR"/../images/priest/clips
+# TexturePacker --trim-mode None --format xml --data "$DIR"/../spritesheets/priest/priest.xml --sheet "$DIR"/../spritesheets/priest/priest.png "$DIR"/../images/priest/clips
+TexturePacker "$DIR"/../images/priest/priest.tps
 
