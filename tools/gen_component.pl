db4d289d0fcb80cbac65749403f9fbbf712bf830
Tanner Evins <tanner@tannerevins.com>
2016-06-21 11:24:45 -0500

    Support `--help' flag in gen_component.pl

--- tools/gen_component.pl
+++ tools/gen_component.pl
@@ -1,8 +1,21 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $usage = "usage: gen_component.pl component_name\n";
-die $usage if @ARGV == 0;
+my $usage = "Usage: gen_component.pl [options] component_name";
+my $help = <<EOF;
+
+$usage
+Options:
+  --help            prints help and exits
+
+Generates to standard output Lua script that interfaces with entity
+state machines. For consistency, any Lua file subsequently created
+should be named like the argument passed here.
+
+EOF
+
+die $usage, "\n" unless @ARGV;
+exit 0 if $ARGV[0] eq "--help" && print $help;
 
 my $component = $ARGV[0];
 
