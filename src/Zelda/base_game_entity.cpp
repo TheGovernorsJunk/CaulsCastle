d25399f038587d1ca3640b613a421604b5bafb4e
Tanner Evins <tanner@tannerevins.com>
2016-06-29 11:56:35 -0500

    Move SceneNode functionality into BaseGameEntity

--- src/Zelda/base_game_entity.cpp
+++ src/Zelda/base_game_entity.cpp
@@ -1,30 +1,52 @@
 #include "base_game_entity.h"
 #include "game.h"
 #include "entity_manager.h"
+#include "shape.h"
+
+#include <cassert>
 
 namespace te
 {
 	BaseGameEntity::BaseGameEntity(Game& world, sf::Vector2f position)
-		: SceneNode(world, position)
-		, mID(UNREGISTERED_ID)
-		, mBoundingRadius(1.f)
-		, mWorld(world)
+		: BaseGameEntity(world)
 	{
 		world.getEntityManager().registerEntity(*this);
 	}
 
 	BaseGameEntity::BaseGameEntity(Game& world, const b2BodyDef& bodyDef)
-		: SceneNode(world, bodyDef)
-		, mID(UNREGISTERED_ID)
-		, mBoundingRadius(1.f)
-		, mWorld(world)
+		: BaseGameEntity(world)
 	{
 		world.getEntityManager().registerEntity(*this);
+
+		mpBody = std::unique_ptr<b2Body, BodyDeleter>{mWorld.getPhysicsWorld().CreateBody(&bodyDef), [this](b2Body* pBody) {
+			mWorld.getPhysicsWorld().DestroyBody(pBody);
+		}};
+		if (!mpBody) throw std::runtime_error("Unable to create b2Body in SceneNode.");
+		mpBody->SetUserData(this);
 	}
+
+	BaseGameEntity::BaseGameEntity(Game& world)
+		: mID(UNREGISTERED_ID)
+		, mZ(0)
+		, mMarkedForRemoval(false)
+		, mBoundingRadius(1.f)
+		, mpBody(nullptr)
+		, mWorld(world)
+	{}
 
 	BaseGameEntity::~BaseGameEntity()
 	{
 		mWorld.getEntityManager().removeEntity(*this);
+	}
+
+	void BaseGameEntity::update(const sf::Time& dt)
+	{
+		if (mpBody)
+		{
+			b2Vec2 pos = mpBody->GetPosition();
+			sf::Transformable::setPosition(pos.x, pos.y);
+		}
+		onUpdate(dt);
 	}
 
 	void BaseGameEntity::setBoundingRadius(float radius)
@@ -57,10 +79,61 @@ namespace te
 		return mWorld;
 	}
 
-	void BaseGameEntity::attachNodeByID(EntityID child)
+	//void BaseGameEntity::attachNodeByID(EntityID child)
+	//{
+	//	BaseGameEntity& entity = getWorld().getEntityManager().getEntityFromID(child);
+	//	std::unique_ptr<SceneNode> pEntity = entity.getParent().detachNode(entity);
+	//	SceneNode::attachNode(std::move(pEntity));
+	//}
+
+	void BaseGameEntity::setDrawOrder(int z)
 	{
-		BaseGameEntity& entity = getWorld().getEntityManager().getEntityFromID(child);
-		std::unique_ptr<SceneNode> pEntity = entity.getParent().detachNode(entity);
-		SceneNode::attachNode(std::move(pEntity));
+		mZ = z;
+	}
+
+	int BaseGameEntity::getDrawOrder() const
+	{
+		return mZ;
+	}
+
+	const sf::Transform& BaseGameEntity::getTransform() const
+	{
+		return sf::Transformable::getTransform();
+	}
+
+	const sf::Vector2f& BaseGameEntity::getPosition() const
+	{
+		return sf::Transformable::getPosition();
+	}
+
+	void BaseGameEntity::attachRigidBody(int bodyType)
+	{
+		assert(bodyType >= 0 && bodyType <= 2);
+		if (!mpBody)
+		{
+			b2BodyDef bodyDef;
+			bodyDef.type = (b2BodyType)bodyType;
+			sf::Vector2f worldPosition = getTransform().transformPoint({ 0, 0 });
+			bodyDef.position = { worldPosition.x, worldPosition.y };
+			mpBody = { mWorld.getPhysicsWorld().CreateBody(&bodyDef), [this](b2Body* pBody) { mWorld.getPhysicsWorld().DestroyBody(pBody); } };
+			mpBody->SetUserData(this);
+		}
+		else
+		{
+			throw std::runtime_error("Node already has rigid body.");
+		}
+	}
+
+	void BaseGameEntity::attachFixture(const Shape* pShape) const
+	{
+		assert(pShape);
+		if (!mpBody) throw std::runtime_error{"Attaching a fixture requires a rigid body."};
+		mpBody->CreateFixture(&pShape->getShape(), 0);
+	}
+
+	void BaseGameEntity::setVelocity(sf::Vector2f vel)
+	{
+		if (!mpBody) throw std::runtime_error{"Setting velocity requires a rigid body."};
+		mpBody->SetLinearVelocity(b2Vec2{vel.x, vel.y});
 	}
 }
--- src/Zelda/base_game_entity.h
+++ src/Zelda/base_game_entity.h
@@ -15,29 +15,55 @@ namespace te
 	struct Telegram;
 	class EntityManager;
 	class Game;
+	class Shape;
 
-	class BaseGameEntity : public SceneNode
+	class BaseGameEntity : private sf::Transformable, public sf::Drawable
 	{
 	public:
 		const static int UNREGISTERED_ID = 0;
 
-		BaseGameEntity(Game& pWorld, sf::Vector2f position = sf::Vector2f{0, 0});
+		BaseGameEntity(Game& pWorld, sf::Vector2f position);
 		BaseGameEntity(Game& pWorld, const b2BodyDef&);
 		virtual ~BaseGameEntity();
 
+		void update(const sf::Time& dt);
 		void setBoundingRadius(float radius);
 		float getBoundingRadius() const;
 		virtual bool handleMessage(const Telegram& msg);
 		EntityID getID() const;
 		const Game& getWorld() const;
 		Game& getWorld();
-		void attachNodeByID(EntityID child);
+		//void attachNodeByID(EntityID child);
+		void setDrawOrder(int z);
+		int getDrawOrder() const;
+		const sf::Transform& getTransform() const;
+		void setPosition(const sf::Vector2f& position) { sf::Transformable::setPosition(position); }
+		const sf::Vector2f& getPosition() const;
+		void move(float x, float y) { sf::Transformable::move(x, y); }
+		void die() { mMarkedForRemoval = true; }
+		bool isMarkedForRemoval() const { return mMarkedForRemoval; }
+
+		// use b2BodyType enum; Lua needs int to interface
+		void attachRigidBody(int);
+		void attachFixture(const Shape* pShape) const;
+		void setVelocity(sf::Vector2f vel);
+
+	protected:
+		virtual void onUpdate(const sf::Time& dt) {}
 
 	private:
 		friend class EntityManager;
 
+		BaseGameEntity(Game& World);
+
+		void draw(sf::RenderTarget&, sf::RenderStates) const {}
+
 		int mID;
+		int mZ;
+		bool mMarkedForRemoval;
 		float mBoundingRadius;
+		using BodyDeleter = std::function<void(b2Body*)>;
+		std::unique_ptr<b2Body, BodyDeleter> mpBody;
 		Game& mWorld;
 	};
 }
