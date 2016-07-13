2515c0e96504876088cff453b0a8442176038d38
Tanner Evins <tanner@tannerevins.com>
2016-07-13 15:12:45 -0500

    Try data-oriented style

--- src/Zelda/main.cpp
+++ src/Zelda/main.cpp
@@ -5,29 +5,69 @@
 #include <iostream>
 #include <Windows.h>
 
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+template <typename Component>
+class ComponentStore
+{
+public:
+	auto begin() { return m_Components.begin(); }
+	auto end() { return m_Components.end(); }
+	auto cbegin() { return m_Components.cbegin(); }
+	auto cend() { return m_Components.cend(); }
+
+	Component& operator[](unsigned index)
+	{
+		if (m_Components.size() <= index) m_Components.resize(index + 1);
+		return m_Components[index];
+	}
+private:
+	std::vector<Component> m_Components;
+};
+
+class IncrementManager
+{
+public:
+	IncrementManager(ComponentStore<sf::Vector2f>& positions)
+		: m_rPositions{positions}
+	{}
+
+	void update()
+	{
+		for (auto& position : m_rPositions)
+		{
+			position += { 1, 1 };
+		}
+	}
+private:
+	ComponentStore<sf::Vector2f>& m_rPositions;
+};
+
 int main(int argc, char* argv[])
 {
-	try
+	ComponentStore<sf::Vector2f> positions{};
+	IncrementManager incrementManager{ positions };
+
+	positions[0] = { -5000, -5000 };
+	positions[3] = { 10, 10 };
+
+	auto pWindow = std::make_unique<sf::RenderWindow>(sf::VideoMode{640, 480}, "Data-Oriented Design");
+	while (pWindow->isOpen())
 	{
-		te::ScriptedApplication app("assets/scripts/config.lua");
-		app.run(app.getConfig().fps);
-		return 0;
+		sf::Event evt;
+		while (pWindow->pollEvent(evt))
+		{
+			if (evt.type == sf::Event::Closed)
+			{
+				pWindow->close();
+			}
+		}
+
+		incrementManager.update();
 	}
-	catch (luabridge::LuaException& ex)
-	{
-		std::cerr << ex.what() << std::endl;
-		return -1;
-	}
-	catch (std::exception& ex)
-	{
-		std::cerr << ex.what() << std::endl;
-		return -1;
-	}
-	catch (...)
-	{
-		std::cerr << "An unknown error occurred." << std::endl;
-		return -1;
-	}
+
+	return 0;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
