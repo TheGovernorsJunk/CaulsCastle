a618863fe042f2b1b9782b84b1cd83dcd4773c4f
Tanner Evins <tanner@tannerevins.com>
2016-08-04 13:37:34 -0500

    Add multi_component

---
+++ src/CaulsCastle/types.h
@@ -0,0 +1,22 @@
+#ifndef TE_TYPES_H
+#define TE_TYPES_H
+
+#include <SDL.h>
+#include <boost/container/flat_map.hpp>
+#include <glm/glm.hpp>
+
+#include <vector>
+
+namespace te
+{
+	using entity_id = int;
+	using vec2 = glm::vec2;
+
+	template <typename K, typename V>
+	using component = boost::container::flat_map<K, V>;
+
+	template <typename K, typename V>
+	using multi_component = std::vector<std::pair<K, V>>;
+}
+
+#endif
