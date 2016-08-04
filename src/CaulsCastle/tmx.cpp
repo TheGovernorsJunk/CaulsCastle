d628503d2fbd00e5139dc5492c0061a717574ba8
Tanner Evins <tanner@tannerevins.com>
2016-08-04 18:02:45 -0500

    Add simplified TMX

---
+++ src/CaulsCastle/tmx.cpp
@@ -0,0 +1,229 @@
+#include "tmx.h"
+
+#include <rapidxml.hpp>
+#include <rapidxml_utils.hpp>
+
+#include <algorithm>
+#include <array>
+#include <set>
+#include <cmath>
+#include <sstream>
+
+namespace te
+{
+	const int TMX::NULL_TILE = -1;
+	const TMX::TileData TMX::NULL_DATA = TMX::TileData{ NULL_TILE, TMX::ObjectGroup() };
+
+	bool TMX::loadFromFile(const std::string& filename)
+	{
+		rapidxml::file<> tmxFile(filename.c_str());
+		rapidxml::xml_document<> tmx;
+		tmx.parse<0>(tmxFile.data());
+
+		mFilename = filename;
+
+		rapidxml::xml_node<char>* pMapNode = tmx.first_node("map");
+
+		std::string orientationStr = pMapNode->first_attribute("orientation")->value();
+		if (orientationStr == "orthogonal") mOrientation = Orientation::Orthogonal;
+		else if (orientationStr == "isometric") mOrientation = Orientation::Isometric;
+		else assert(false);
+
+		width = std::stoi(pMapNode->first_attribute("width")->value());
+		height = std::stoi(pMapNode->first_attribute("height")->value());
+		tilewidth = std::stoi(pMapNode->first_attribute("tilewidth")->value());
+		tileheight = std::stoi(pMapNode->first_attribute("tileheight")->value());
+
+		for (rapidxml::xml_node<char>* pTileset = tmx.first_node("map")->first_node("tileset"); pTileset != 0; pTileset = pTileset->next_sibling("tileset"))
+		{
+			std::vector<TileData> tiles;
+			for (rapidxml::xml_node<char>* pTile = pTileset->first_node("tile"); pTile != 0; pTile = pTile->next_sibling("tile"))
+			{
+				rapidxml::xml_node<char>* pObjectGroup = pTile->first_node("objectgroup");
+				if (pObjectGroup != 0)
+				{
+					std::vector<Object> objects;
+					for (rapidxml::xml_node<char>* pObject = pObjectGroup->first_node("object"); pObject != 0; pObject = pObject->next_sibling("object"))
+					{
+						std::vector<Polygon> polygons;
+						for (rapidxml::xml_node<char>* pPolygon = pObject->first_node("polygon"); pPolygon != 0; pPolygon = pPolygon->next_sibling("polygon"))
+						{
+							decltype(polygons[0].points) pointsVec{};
+
+							std::string pointsStr(pPolygon->first_attribute("points")->value());
+							std::istringstream iss(pointsStr);
+							std::vector<std::string> points{ std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{} };
+							for (auto& point : points)
+							{
+								std::stringstream ss(point);
+								std::string xCoord;
+								std::getline(ss, xCoord, ',');
+								std::string yCoord;
+								std::getline(ss, yCoord);
+								pointsVec.push_back({ std::stof(xCoord), std::stof(yCoord) });
+							}
+
+							polygons.push_back({ std::move(pointsVec) });
+						}
+
+						objects.push_back(Object{
+							std::stoi(pObject->first_attribute("id")->value()),
+							"",
+							"",
+							std::stoi(pObject->first_attribute("x")->value()),
+							std::stoi(pObject->first_attribute("y")->value()),
+							pObject->first_attribute("width") != 0 ? std::stoi(pObject->first_attribute("width")->value()) : 0,
+							pObject->first_attribute("height") != 0 ? std::stoi(pObject->first_attribute("height")->value()) : 0,
+							std::move(polygons)
+						});
+					}
+
+					tiles.push_back({
+						std::stoi(pTile->first_attribute("id")->value()),
+						ObjectGroup {
+							"",
+							pObjectGroup->first_attribute("draworder")->value(),
+							std::move(objects),
+							0
+						}
+					});
+				}
+				else
+				{
+					tiles.push_back({
+						std::stoi(pTile->first_attribute("id")->value()),
+						ObjectGroup {}
+					});
+				}
+			}
+
+			rapidxml::xml_node<char>* pImage = pTileset->first_node("image");
+
+			tilesets.push_back({
+				std::stoi(pTileset->first_attribute("firstgid")->value()),
+				pTileset->first_attribute("name")->value(),
+				std::stoi(pTileset->first_attribute("tilewidth")->value()),
+				std::stoi(pTileset->first_attribute("tileheight")->value()),
+				std::stoi(pTileset->first_attribute("tilecount")->value()), {
+					pImage->first_attribute("source")->value(),
+					std::stoi(pImage->first_attribute("width")->value()),
+				    std::stoi(pImage->first_attribute("height")->value())
+				},
+				std::move(tiles)
+			});
+		}
+
+		rapidxml::xml_node<char>* pLastTileset = nullptr;
+		for (auto* pTileset = tmx.first_node("map")->first_node("tileset"); pTileset != 0; pTileset = pTileset->next_sibling("tileset"))
+			pLastTileset = pTileset;
+		Index currIndex = 0;
+		std::vector<Index> tileLayerIndices;
+		std::vector<Index> objectLayerIndices;
+		for (auto* pLayer = pLastTileset->next_sibling(); pLayer != 0; pLayer = pLayer->next_sibling())
+		{
+			if (std::string(pLayer->name()) == "layer")
+				tileLayerIndices.push_back(currIndex);
+			if (std::string(pLayer->name()) == "objectgroup")
+				objectLayerIndices.push_back(currIndex);
+			++currIndex;
+		}
+
+		auto layerIndexIter = tileLayerIndices.begin();
+		for (rapidxml::xml_node<char>* pLayer = tmx.first_node("map")->first_node("layer"); pLayer != 0; pLayer = pLayer->next_sibling("layer"))
+		{
+			std::vector<Tile> tiles;
+			for (rapidxml::xml_node<char>* pTile = pLayer->first_node("data")->first_node("tile"); pTile != 0; pTile = pTile->next_sibling("tile"))
+			{
+				tiles.push_back({
+					std::stoi(pTile->first_attribute("gid")->value())
+				});
+			}
+			layers.push_back({
+				pLayer->first_attribute("name")->value(),
+				std::stoi(pLayer->first_attribute("width")->value()),
+				std::stoi(pLayer->first_attribute("height")->value()),
+				{std::move(tiles)},
+				*layerIndexIter++
+			});
+		}
+
+		auto objectIndexIter = objectLayerIndices.begin();
+		for (rapidxml::xml_node<char>* pObjectgroup = tmx.first_node("map")->first_node("objectgroup"); pObjectgroup != 0; pObjectgroup = pObjectgroup->next_sibling("objectgroup"))
+		{
+			std::vector<Object> objects;
+			for (rapidxml::xml_node<char>* pObject = pObjectgroup->first_node("object"); pObject != 0; pObject = pObject->next_sibling("object"))
+			{
+				objects.push_back({
+					std::stoi(pObject->first_attribute("id")->value()),
+					pObject->first_attribute("name") != 0 ? pObject->first_attribute("name")->value() : "",
+					pObject->first_attribute("type") != 0 ? pObject->first_attribute("type")->value() : "",
+					std::stoi(pObject->first_attribute("x")->value()),
+					std::stoi(pObject->first_attribute("y")->value()),
+					pObject->first_attribute("width") != 0 ? std::stoi(pObject->first_attribute("width")->value()) : 0,
+					pObject->first_attribute("height") != 0 ? std::stoi(pObject->first_attribute("height")->value()) : 0
+				});
+			}
+			objectgroups.push_back({
+				pObjectgroup->first_attribute("name")->value(),
+				"",
+				std::move(objects),
+				*objectIndexIter++
+			});
+		}
+
+		// This is some accidental duplication
+		rapidxml::xml_node<char>* pLayerOrObjectNode = tmx.first_node("map")->first_node("tileset")->next_sibling();
+		while (pLayerOrObjectNode != 0)
+		{
+			std::string nodeName{pLayerOrObjectNode->name()};
+			if (nodeName == "layer" || nodeName == "objectgroup")
+			{
+				layernames.push_back(pLayerOrObjectNode->first_attribute("name")->value());
+			}
+			pLayerOrObjectNode = pLayerOrObjectNode->next_sibling();
+		}
+
+		return true;
+	}
+
+	size_t TMX::getTilesetIndex(int gid) const
+	{
+		auto retIt = tilesets.end();
+		for (auto it = tilesets.begin(); it != tilesets.end(); ++it)
+		{
+			if (gid >= it->firstgid && gid < it->firstgid + it->tilecount)
+			{
+				retIt = it;
+			}
+		}
+		assert(retIt != tilesets.end());
+		return retIt - tilesets.begin();
+	}
+
+	const TMX::TileData& TMX::getTileData(int x, int y, const TMX::Layer& layer) const
+	{
+		int i = y * width + x;
+		assert(i < width * height);
+		const Tile& tile = layer.data.tiles[i];
+		if (tile.gid == 0)
+		{
+			return TMX::NULL_DATA;
+		}
+		const Tileset& tileset = tilesets[getTilesetIndex(tile.gid)];
+		const int localId = tile.gid - tileset.firstgid;
+		const std::vector<TileData>& tiles = tileset.tiles;
+		auto result = std::find_if(tiles.begin(), tiles.end(), [localId](const TileData& data) {
+			return localId == data.id;
+		});
+		if (result == tiles.end())
+		{
+			return TMX::NULL_DATA;
+		}
+		return *result;
+	}
+
+	int TMX::index(int x, int y) const
+	{
+		return y * width + x;
+	}
+}
---
+++ src/CaulsCastle/tmx.h
@@ -0,0 +1,102 @@
+#ifndef TE_TMX_H
+#define TE_TMX_H
+
+#include "types.h"
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+namespace te
+{
+	struct TMX
+	{
+		using Index = size_t;
+
+		enum class Orientation {
+			Orthogonal,
+			Isometric
+		};
+
+		struct Polygon {
+			std::vector<vec2> points;
+		};
+		struct Object {
+			int id;
+			std::string name;
+			std::string type;
+			int x;
+			int y;
+			int width;
+			int height;
+			std::vector<Polygon> polygons;
+		};
+		struct ObjectGroup {
+			std::string name;
+			std::string draworder;
+			std::vector<Object> objects;
+			Index index;
+		};
+		struct Image {
+			std::string source;
+			int width;
+			int height;
+		};
+		struct TileData {
+			int id;
+			ObjectGroup objectgroup;
+		};
+		struct Tileset {
+			int firstgid;
+			std::string name;
+			int tilewidth;
+			int tileheight;
+			int tilecount;
+			Image image;
+			std::vector<TileData> tiles;
+		};
+		struct Tile {
+			int gid;
+		};
+
+		class Data {
+			friend struct TMX;
+			std::vector<Tile> tiles;
+			Data(std::vector<Tile>&& ts) : tiles(std::move(ts)) {}
+		public:
+			using ConstTileIterator = std::vector<Tile>::const_iterator;
+			ConstTileIterator begin() const { return tiles.begin(); }
+			ConstTileIterator end() const { return tiles.end(); }
+		};
+		struct Layer {
+			std::string name;
+			int width;
+			int height;
+			Data data;
+			Index index;
+		};
+
+		bool loadFromFile(const std::string& filename);
+
+		static const int NULL_TILE;
+		static const TileData NULL_DATA;
+
+		size_t getTilesetIndex(int gid) const;
+		const TileData& getTileData(int x, int y, const Layer& layer) const;
+		int index(int x, int y) const;
+
+		std::string mFilename;
+		Orientation mOrientation;
+		int width;
+		int height;
+		int tilewidth;
+		int tileheight;
+		std::vector<Tileset> tilesets;
+		std::vector<Layer> layers;
+		std::vector<ObjectGroup> objectgroups;
+		std::vector<std::string> layernames;
+	};
+}
+
+#endif
