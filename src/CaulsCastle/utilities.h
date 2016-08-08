28420d2e46a5d3e37df4b6c7d2551631dbbc8ef0
Tanner Evins <tanner@tannerevins.com>
2016-08-08 16:13:21 -0500

    Add get_directory utility function

--- src/CaulsCastle/utilities.h
+++ src/CaulsCastle/utilities.h
@@ -1,6 +1,9 @@
 #ifndef TE_UTILITIES_H
 #define TE_UTILITIES_H
 
+#include <algorithm>
+#include <string>
+
 namespace te
 {
 	inline int pow2up(int x)
@@ -12,6 +15,12 @@ namespace te
 		x |= x >> 8;
 		x |= x >> 16;
 		return ++x;
+	}
+
+	inline std::string get_directory(const std::string& filename, const std::string& delimiter = "/")
+	{
+		auto result = std::find_end(filename.begin(), filename.end(), delimiter.begin(), delimiter.end());
+		return result != filename.end() ? std::string(filename.begin(), result + 1) : "";
 	}
 }
 
