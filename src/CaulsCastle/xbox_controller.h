d44e9c9704a5dcfeef5ca5de47fe811dadf33422
Tanner Evins <tanner@tannerevins.com>
2016-08-08 21:07:32 -0500

    Add Xbox controller input enums

---
+++ src/CaulsCastle/xbox_controller.h
@@ -0,0 +1,33 @@
+#ifndef TE_XBOX_CONTROLLER_H
+#define TE_XBOX_CONTROLLER_H
+
+namespace te
+{
+	enum Xbox_button {
+		A = 10,
+		B = 11,
+		X = 12,
+		Y = 13,
+		LB = 8,
+		RB = 9,
+		Back = 5,
+		Start = 4,
+		L3 = 6,
+		R3 = 7,
+		D_up = 0,
+		D_down = 1,
+		D_left = 2,
+		D_right = 3
+	};
+
+	enum Xbox_axis {
+		X = 0,
+		Y = 1,
+		U = 2,
+		V = 3,
+		LT = 4,
+		RT = 5
+	};
+}
+
+#endif
