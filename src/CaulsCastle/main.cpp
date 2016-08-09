c6fa65d54b3322bc5d6072b76ff27cc8ffd2f7cf
Tanner Evins <tanner@tannerevins.com>
2016-08-08 23:52:12 -0500

    Add alpha blending

--- src/CaulsCastle/main.cpp
+++ src/CaulsCastle/main.cpp
@@ -121,6 +121,8 @@ int main(int argc, char** argv)
 	glLoadIdentity();
 	assert(glGetError() == GL_NO_ERROR);
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_TEXTURE_2D);
 
 	glClearColor(0, 0, 0.2f, 1.f);
