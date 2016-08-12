1e82907e948135ef60b59aa0963ceefd79695f09
Tanner Evins <tanner@tannerevins.com>
2016-08-12 14:12:44 -0500

    Add and call void clear(Button_state&)

--- src/CaulsCastle/input.cpp
+++ src/CaulsCastle/input.cpp
@@ -2,13 +2,18 @@
 
 namespace te {
 
+static inline void clear(Player_input::Button_state& state)
+{
+	state.fire = false;
+	state.release = false;
+}
+
 void clear(Player_input& input)
 {
 	input.x_movement = 0;
 	input.y_movement = 0;
 
-	input.dodge.fire = false;
-	input.dodge.release = false;
+	clear(input.dodge);
 }
 
 } // namespace te
