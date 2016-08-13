d7c02941a913b62b5cd51f7cd17dacb2c6033ceb
Tanner Evins <Tanner Evins>
2016-08-13 11:35:22 -0500

    Add make_meshes with atlas parameter

--- src/CaulsCastle/texture_atlas.h
+++ src/CaulsCastle/texture_atlas.h
@@ -1,6 +1,8 @@
 #ifndef TE_TEXTURE_ATLAS_H
 #define TE_TEXTURE_ATLAS_H
 
+#include <SDL_opengl.h>
+
 #include <string>
 #include <vector>
 
@@ -24,6 +26,44 @@ struct Texture_atlas {
 
 	Texture_atlas(const char* path);
 };
+
+template <typename Container, template <typename> typename Iter>
+void make_meshes(const Texture_atlas& atlas, GLuint texture_id, Iter<Container> out)
+{
+	using Mesh = Container::value_type;
+	using Vertex = decltype(Mesh::vertices)::value_type;
+	using PositionCoord = decltype(Vertex::position_value_type::x);
+	using TexCoord = decltype(Vertex::tex_value_type::x);
+
+	glBindTexture(GL_TEXTURE_2D,texture_id);
+
+	GLint tex_width, tex_height;
+	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &tex_width);
+	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &tex_height);
+
+	for (auto& sprite : atlas.sprites) {
+		decltype(Mesh::vertices) quad(4);
+		quad[0].tex_coords = { (TexCoord)(sprite.x) / tex_width,
+				       (TexCoord)(sprite.y) / tex_height };
+		quad[1].tex_coords = { (TexCoord)(sprite.x + sprite.w) / tex_width,
+				       (TexCoord)(sprite.y) / tex_height };
+		quad[2].tex_coords = { (TexCoord)(sprite.x + sprite.w) / tex_width,
+				       (TexCoord)(sprite.y + sprite.h) / tex_height };
+		quad[3].tex_coords = { (TexCoord)(sprite.x) / tex_width,
+				       (TexCoord)(sprite.y + sprite.h) / tex_height };
+
+		quad[0].position = { (PositionCoord)sprite.x,
+				     (PositionCoord)sprite.y };
+		quad[1].position = { (PositionCoord)(sprite.x + sprite.w),
+				     (PositionCoord)sprite.y };
+		quad[2].position = { (PositionCoord)(sprite.x + sprite.w),
+				     (PositionCoord)(sprite.y + sprite.h) };
+		quad[3].position = { (PositionCoord)(sprite.x),
+				     (PositionCoord)(sprite.y + sprite.h) };
+
+		out++ = { std::move(quad), texture_id, GL_QUADS };
+	}
+}
 
 } // namespace te
 
