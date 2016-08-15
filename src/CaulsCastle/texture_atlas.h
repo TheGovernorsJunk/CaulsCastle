ae1b14adf0b3d131ce3a055f5c73e59fbeb6c88b
Tanner Evins <tanner@tannerevins.com>
2016-08-15 09:59:01 -0500

    Depend on make_mesh in make_meshes

--- src/CaulsCastle/texture_atlas.h
+++ src/CaulsCastle/texture_atlas.h
@@ -1,6 +1,9 @@
 #ifndef TE_TEXTURE_ATLAS_H
 #define TE_TEXTURE_ATLAS_H
 
+#include "mesh.h"
+#include "records.h"
+
 #include <SDL_opengl.h>
 
 #include <string>
@@ -30,40 +33,18 @@ struct Texture_atlas {
 template <typename Container, template <typename> typename Iter>
 void make_meshes(const Texture_atlas& atlas, GLuint texture_id, Iter<Container> out)
 {
-	using Mesh = Container::value_type;
-	using Vertex = decltype(Mesh::vertices)::value_type;
-	using PositionCoord = decltype(Vertex::position_value_type::x);
-	using TexCoord = decltype(Vertex::tex_value_type::x);
-
-	glBindTexture(GL_TEXTURE_2D, texture_id);
-
-	GLint tex_width, tex_height;
-	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &tex_width);
-	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &tex_height);
-
-	glBindTexture(GL_TEXTURE_2D, NULL);
-
 	for (auto& sprite : atlas.sprites) {
-		decltype(Mesh::vertices) quad(4);
-		quad[0].tex_coords = { (TexCoord)(sprite.x) / tex_width,
-				       (TexCoord)(sprite.y) / tex_height };
-		quad[1].tex_coords = { (TexCoord)(sprite.x + sprite.w) / tex_width,
-				       (TexCoord)(sprite.y) / tex_height };
-		quad[2].tex_coords = { (TexCoord)(sprite.x + sprite.w) / tex_width,
-				       (TexCoord)(sprite.y + sprite.h) / tex_height };
-		quad[3].tex_coords = { (TexCoord)(sprite.x) / tex_width,
-				       (TexCoord)(sprite.y + sprite.h) / tex_height };
-
-		quad[0].position = { (PositionCoord)0,
-				     (PositionCoord)0 };
-		quad[1].position = { (PositionCoord)sprite.w,
-				     (PositionCoord)0 };
-		quad[2].position = { (PositionCoord)sprite.w,
-				     (PositionCoord)sprite.h };
-		quad[3].position = { (PositionCoord)0,
-				     (PositionCoord)sprite.h };
-
-		out++ = { std::move(quad), texture_id, GL_QUADS };
+		Sprite_record record{
+			sprite.n,
+			atlas.imagePath,
+			sprite.x,
+			sprite.y,
+			sprite.w,
+			sprite.h,
+			sprite.pX,
+			sprite.pY
+		};
+		out++ = make_mesh(record, texture_id);
 	}
 }
 
