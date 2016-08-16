1bd1acb6989b39bacdda1dc8fa1ba627397c278c
Tanner Evins <Tanner Evins>
2016-08-16 16:42:38 -0500

    Insert default keymap for first player

--- src/CaulsCastle/main.cpp
+++ src/CaulsCastle/main.cpp
@@ -144,6 +144,7 @@ int main(int argc, char** argv)
 
 	if (p_joystick) {
 		data.controllers.insert(std::pair<Player_id, decltype(Game_data::controllers)::mapped_type>{ 0, std::move(p_joystick) });
+		data.keymaps.insert(decltype(data.keymaps)::value_type{ 0, Keymap{} });
 	}
 
 	data.pixel_to_world_scale = pixel_to_world_scale;
