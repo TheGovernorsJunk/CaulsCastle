adb67206814af97adb97d4cfbb12fb915a8407d7
Tanner Evins <Tanner Evins>
2016-08-20 17:05:21 -0500

    Use lookup table to set attack animation

--- src/CaulsCastle/light_attack_state.cpp
+++ src/CaulsCastle/light_attack_state.cpp
@@ -8,25 +8,17 @@ void Light_attack_state_table::step_entering(Record_type& record, Game_data& dat
 {
 	const auto& animation_group = data.entity_animation_groups[record.id];
 	auto& animation = data.entity_animations2[record.id];
+	auto heading = data.headings[record.id];
 
-	const auto assign_if = [&animation](bool condition, Resource_id<Animation2> id)
-	{
-		if (condition) {
-			animation.id = id;
-			animation.frame_index = 0;
-			animation.t = 0;
-		}
-	};
-
-	auto curr_anim = animation.id;
-	assign_if(curr_anim == animation_group.idle_left || curr_anim == animation_group.walk_left,
-		  animation_group.light_attack_left);
-	assign_if(curr_anim == animation_group.idle_right || curr_anim == animation_group.walk_right,
-		  animation_group.light_attack_right);
-	assign_if(curr_anim == animation_group.idle_up || curr_anim == animation_group.walk_up,
-		  animation_group.light_attack_up);
-	assign_if(curr_anim == animation_group.idle_down || curr_anim == animation_group.walk_down,
-		  animation_group.light_attack_down);
+	animation.id = animation_group.lookup_table.get(Entity_animation::Query(
+		true,
+		{},
+		{},
+		std::abs(heading.x) > std::abs(heading.y),
+		heading.x > 0,
+		heading.y > 0));
+	animation.frame_index = 0;
+	animation.t = 0;
 
 	auto duration_found = m_animation_durations.find(animation.id);
 	if (duration_found == m_animation_durations.end()) {
