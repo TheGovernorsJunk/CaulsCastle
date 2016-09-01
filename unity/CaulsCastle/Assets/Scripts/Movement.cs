5a21e4cc3b2c706dcd449fd2ca0d41aa5c3c8d75
Tanner Evins <Tanner Evins>
2016-09-01 18:30:57 -0500

    Add Movement script

---
+++ unity/CaulsCastle/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private Rigidbody2D m_rigidbody;
+	private Vector2 m_direction;
+
+	void Start()
+	{
+		m_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+		m_direction = new Vector2(x, y);
+	}
+
+	void FixedUpdate()
+	{
+		m_rigidbody.velocity = m_direction * 5;
+	}
+}
