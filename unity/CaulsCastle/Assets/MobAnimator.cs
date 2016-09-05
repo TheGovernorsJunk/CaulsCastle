ceee2b1c986a0cb56b2709fcce0b3cfd7a151ff9
Tanner Evins <Tanner Evins>
2016-09-05 14:02:29 -0500

    Break animator script apart from movement

---
+++ unity/CaulsCastle/Assets/MobAnimator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobAnimator : MonoBehaviour {
+
+	int UP = Animator.StringToHash("up");
+	int DOWN = Animator.StringToHash("down");
+	int LEFT = Animator.StringToHash("left");
+	int RIGHT = Animator.StringToHash("right");
+	int SPEED = Animator.StringToHash("speed");
+
+	Movement m_movement;
+	Animator m_animator;
+
+	void Start ()
+	{
+		m_movement = GetComponent<Movement>();
+		m_animator = GetComponent<Animator>();
+	}
+	
+	void FixedUpdate ()
+	{
+		Vector2 heading = m_movement.Heading;
+		float x = heading.x;
+		float y = heading.y;
+		if (x != 0 || y != 0)
+		{
+			if (Mathf.Abs(x) > Mathf.Abs(y))
+			{
+				m_animator.SetTrigger(x > 0 ? RIGHT : LEFT);
+			}
+			else
+			{
+				m_animator.SetTrigger(y > 0 ? UP : DOWN);
+			}
+			m_animator.SetInteger(SPEED, 1);
+		}
+		else
+		{
+			m_animator.SetInteger(SPEED, 0);
+		}
+	}
+}
--- unity/CaulsCastle/Assets/Scripts/Movement.cs
+++ unity/CaulsCastle/Assets/Scripts/Movement.cs
@@ -12,41 +12,14 @@ public class Movement : MonoBehaviour {
 
 	private Rigidbody2D m_rigidbody;
 	private Vector2 m_heading;
-	private Animator m_animator;
-
-	private int UP = Animator.StringToHash("up");
-	private int DOWN = Animator.StringToHash("down");
-	private int LEFT = Animator.StringToHash("left");
-	private int RIGHT = Animator.StringToHash("right");
-	private int SPEED = Animator.StringToHash("speed");
 
 	void Start()
 	{
 		m_rigidbody = GetComponent<Rigidbody2D>();
-		m_animator = GetComponent<Animator>();
 	}
 
 	void FixedUpdate()
 	{
 		m_rigidbody.velocity = m_heading * Speed;
-
-		float x = m_heading.x;
-		float y = m_heading.y;
-		if (x != 0 || y != 0)
-		{
-			if (Mathf.Abs(x) > Mathf.Abs(y))
-			{
-				m_animator.SetTrigger(x > 0 ? RIGHT : LEFT);
-			}
-			else
-			{
-				m_animator.SetTrigger(y > 0 ? UP : DOWN);
-			}
-			m_animator.SetInteger(SPEED, 1);
-		}
-		else
-		{
-			m_animator.SetInteger(SPEED, 0);
-		}
 	}
 }
