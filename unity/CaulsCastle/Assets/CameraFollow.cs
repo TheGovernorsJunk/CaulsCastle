5871396fa0ce83f727339b8e3d49228b645297e8
Tanner Evins <Tanner Evins>
2016-09-06 16:40:33 -0500

    Add CameraFollow

---
+++ unity/CaulsCastle/Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public GameObject Subject;
+
+	void Update ()
+	{
+		Vector2 position = Subject.transform.position;
+		transform.position = new Vector3(position.x, position.y, transform.position.z);
+	}
+}
