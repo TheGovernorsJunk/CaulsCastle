6da0d1e4858899d89a79eee69314679b1b5eb582
Tanner Evins <Tanner Evins>
2016-09-06 17:11:45 -0500

    Add lerping behavior to camera

--- unity/CaulsCastle/Assets/CameraFollow.cs
+++ unity/CaulsCastle/Assets/CameraFollow.cs
@@ -5,10 +5,19 @@ public class CameraFollow : MonoBehaviour
 {
 
 	public GameObject Subject;
+	public float Distance = 1.0f;
+
+	Movement m_subject_movement;
+
+	void Awake ()
+	{
+		m_subject_movement = Subject.GetComponent<Movement>();
+	}
 
 	void Update ()
 	{
 		Vector2 position = Subject.transform.position;
-		transform.position = new Vector3(position.x, position.y, transform.position.z);
+		position = position + (m_subject_movement.Heading * Distance);
+		transform.position = Vector3.Lerp(transform.position, new Vector3(position.x, position.y, transform.position.z), 0.1f);
 	}
 }
