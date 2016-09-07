2f59f10a872ca8451b6dbeb6b7ce36211303cf10
Tanner Evins <tanner@tannerevins.com>
2016-09-07 17:07:04 -0500

    Add LockOn

---
+++ unity/CaulsCastle/Assets/LockOn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockOn : MonoBehaviour {
+
+	public LayerMask LockMask = -1;
+	public float LockDistance = 5f;
+	public string LockTag = "Enemy";
+
+	Movement m_movement;
+
+	void Awake()
+	{
+		m_movement = GetComponent<Movement>();
+	}
+
+	public void Trigger(Vector2 direction)
+	{
+		RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction, LockDistance, LockMask.value);
+		foreach (RaycastHit2D hit in hits)
+		{
+			if (hit.collider.CompareTag(LockTag))
+			{
+				Debug.Log("Lock!");
+				break;
+			}
+		}
+	}
+}
