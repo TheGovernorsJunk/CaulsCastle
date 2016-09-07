f8b0b4ae8da3d8195fcb7582ce6743e421220037
Tanner Evins <Tanner Evins>
2016-09-06 20:50:04 -0500

    Add LightAttack.cs

---
+++ unity/CaulsCastle/Assets/Scripts/LightAttack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightAttack : MonoBehaviour {
+
+	static int ATTACK = Animator.StringToHash("attack");
+
+	Animator m_animator;
+
+	void Awake()
+	{
+		m_animator = GetComponent<Animator>();
+	}
+
+	public void Trigger()
+	{
+		if (enabled) m_animator.SetTrigger(ATTACK);
+	}
+}
