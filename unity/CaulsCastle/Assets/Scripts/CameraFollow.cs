59c6e0fa523671b5152eef3a4993fedad9e36253
Tanner Evins <Tanner Evins>
2016-09-07 20:59:44 -0500

    Implement target camera

--- unity/CaulsCastle/Assets/Scripts/CameraFollow.cs
+++ unity/CaulsCastle/Assets/Scripts/CameraFollow.cs
@@ -8,16 +8,27 @@ public class CameraFollow : MonoBehaviour
 	public float Distance = 1.0f;
 
 	Movement m_subject_movement;
+	LockOn m_subject_lock_on;
 
 	void Awake ()
 	{
 		m_subject_movement = Subject.GetComponent<Movement>();
+		m_subject_lock_on = Subject.GetComponent<LockOn>();
 	}
 
 	void Update ()
 	{
-		Vector2 position = Subject.transform.position;
-		position = position + (m_subject_movement.Heading * Distance);
-		transform.position = Vector3.Lerp(transform.position, new Vector3(position.x, position.y, transform.position.z), 0.1f);
+		GameObject target = m_subject_lock_on.Target;
+		if (target == null)
+		{
+			Vector2 position = Subject.transform.position;
+			position = position + (m_subject_movement.Heading * Distance);
+			transform.position = Vector3.Lerp(transform.position, new Vector3(position.x, position.y, transform.position.z), 0.1f);
+		}
+		else
+		{
+			Vector2 midway = Vector2.Lerp(target.transform.position, Subject.transform.position, 0.5f);
+			transform.position = Vector3.Lerp(transform.position, new Vector3(midway.x, midway.y, transform.position.z), 0.1f);
+		}
 	}
 }
