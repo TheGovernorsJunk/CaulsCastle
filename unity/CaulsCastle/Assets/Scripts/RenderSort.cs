f12ce642092548d9f5c2059a725fbd32aefc9033
Tanner Evins <Tanner Evins>
2016-09-08 22:26:58 -0500

    Add RenderSort to sort sprites by y-coordinate

---
+++ unity/CaulsCastle/Assets/Scripts/RenderSort.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderSort : MonoBehaviour
+{
+	SpriteRenderer mRenderer;
+
+	void Awake ()
+	{
+		mRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void LateUpdate ()
+	{
+		mRenderer.sortingOrder = (int)Camera.main.WorldToScreenPoint(mRenderer.bounds.min).y * -1;
+	}
+}
