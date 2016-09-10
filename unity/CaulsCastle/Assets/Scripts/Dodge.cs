73a79ade2ba98f8274a197beb6ef548fb4b30e69
Tanner Evins <Tanner Evins>
2016-09-10 11:13:35 -0500

    Add Dodge component, add to Hero

---
+++ unity/CaulsCastle/Assets/Scripts/Dodge.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dodge : MonoBehaviour
+{
+
+	static int DODGE = Animator.StringToHash("dodge");
+
+	Animator mAnimator;
+
+	void Awake()
+	{
+		mAnimator = GetComponent<Animator>();
+	}
+
+	public void Trigger()
+	{
+		if (!enabled) return;
+		mAnimator.SetTrigger(DODGE);
+	}
+}
