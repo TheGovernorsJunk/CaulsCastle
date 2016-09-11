238eabf1b3cb0d01b781789545f3f1d829c74541
Tanner Evins <Tanner Evins>
2016-09-11 14:06:07 -0500

    Allow player to lock on

--- unity/CaulsCastle/Assets/Scripts/PlayerMotionInput.cs
+++ unity/CaulsCastle/Assets/Scripts/PlayerMotionInput.cs
@@ -6,13 +6,22 @@ public class PlayerMotionInput : MonoBehaviour
 	public GameObject Avatar;
 
 	MobMotion mMobMotion;
+	LockOn mLockOn;
+	Animator mAnimator;
+
+	static float LockInfluenceThreshold = 0.3f;
+	static int NormalX = Animator.StringToHash("NormalX");
+	static int NormalY = Animator.StringToHash("NormalY");
 
 	void Awake()
 	{
 		mMobMotion = Avatar.GetComponent<MobMotion>();
+		mLockOn = Avatar.GetComponent<LockOn>();
+		mAnimator = Avatar.GetComponent<Animator>();
 	}
 
 	bool attackDown = false;
+	bool lockDown = false;
 	void Update()
 	{
 		float x = Input.GetAxisRaw("Horizontal");
@@ -27,6 +36,24 @@ public class PlayerMotionInput : MonoBehaviour
 		else if (attackInput == 0)
 		{
 			attackDown = false;
+		}
+
+		float lockInput = Input.GetAxisRaw("Lock");
+		if (lockInput > 0 && !lockDown)
+		{
+			float lockX = Input.GetAxisRaw("U");
+			float lockY = Input.GetAxisRaw("V");
+			float headingX = mAnimator.GetFloat(NormalX);
+			float headingY = mAnimator.GetFloat(NormalY);
+			Vector2 lockDirection = Mathf.Abs(lockX) > LockInfluenceThreshold || Mathf.Abs(lockY) > LockInfluenceThreshold
+				? new Vector2(lockX, lockY)
+				: new Vector2(headingX, headingY);
+			mLockOn.Trigger(lockDirection);
+			lockDown = true;
+		}
+		else if (lockInput == 0)
+		{
+			lockDown = false;
 		}
 	}
 }
