e8fd7b14c01106c01ee1c727fc77791fa011710e
Tanner Evins <Tanner Evins>
2016-09-11 11:21:42 -0500

    Add MobMotion to apply root motion

---
+++ unity/CaulsCastle/Assets/Scripts/MobMotion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class MobMotion : MonoBehaviour
+{
+	Animator mAnimator;
+	Rigidbody2D mRigidbody;
+
+	void Awake()
+	{
+		mAnimator = GetComponent<Animator>();
+		mRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void OnAnimatorMove()
+	{
+		mRigidbody.velocity = mAnimator.deltaPosition / Time.deltaTime;
+	}
+}
