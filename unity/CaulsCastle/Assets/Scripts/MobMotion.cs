0bcf3c821a2a6b7ac119597b79acb83686a551ee
Tanner Evins <Tanner Evins>
2016-09-11 14:05:04 -0500

    Send lock-on information to animator

--- unity/CaulsCastle/Assets/Scripts/MobMotion.cs
+++ unity/CaulsCastle/Assets/Scripts/MobMotion.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(LockOn))]
 
 public class MobMotion : MonoBehaviour
 {
+	static int HasTarget = Animator.StringToHash("HasTarget");
 	Animator mAnimator;
 	Rigidbody2D mRigidbody;
+	LockOn mLockOn;
 
 	public Vector2 Movement { get; set; }
 	public bool Attack { get; set; }
@@ -16,6 +19,23 @@ public class MobMotion : MonoBehaviour
 	{
 		mAnimator = GetComponent<Animator>();
 		mRigidbody = GetComponent<Rigidbody2D>();
+		mLockOn = GetComponent<LockOn>();
+		mLockOn.LockEvent.AddListener(HandleLock);
+	}
+
+	void HandleLock()
+	{
+		StartCoroutine(Locking());
+	}
+
+	IEnumerator Locking()
+	{
+		while (mLockOn.Target)
+		{
+			mAnimator.SetBool(HasTarget, true);
+			yield return null;
+		}
+		mAnimator.SetBool(HasTarget, false);
 	}
 
 	void OnAnimatorMove()
