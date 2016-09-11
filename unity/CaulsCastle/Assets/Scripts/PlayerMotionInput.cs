dcbe20fbce1645fea99d17c98afcc81718e13429
Tanner Evins <Tanner Evins>
2016-09-11 12:55:37 -0500

    Assign MobMotion.Attack in PlayerMotionInput

--- unity/CaulsCastle/Assets/Scripts/PlayerMotionInput.cs
+++ unity/CaulsCastle/Assets/Scripts/PlayerMotionInput.cs
@@ -11,11 +11,22 @@ public class PlayerMotionInput : MonoBehaviour
 	{
 		mMobMotion = Avatar.GetComponent<MobMotion>();
 	}
-	
+
+	bool attackDown = false;
 	void Update()
 	{
 		float x = Input.GetAxisRaw("Horizontal");
 		float y = Input.GetAxisRaw("Vertical");
 		mMobMotion.Movement = new Vector2(x, y);
+
+		float attackInput = Input.GetAxisRaw("LightAttack");
+		if (attackInput > 0 && !attackDown)
+		{
+			mMobMotion.Attack = attackDown = true;
+		}
+		else if (attackInput == 0)
+		{
+			attackDown = false;
+		}
 	}
 }
