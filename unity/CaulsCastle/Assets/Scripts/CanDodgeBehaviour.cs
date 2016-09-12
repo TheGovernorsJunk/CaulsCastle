969684c486b56e3ec9dd03f195274ac032605db7
Tanner Evins <Tanner Evins>
2016-09-11 22:25:53 -0500

    Add CanDodgeBehaviour

---
+++ unity/CaulsCastle/Assets/Scripts/CanDodgeBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CanDodgeBehaviour : StateMachineBehaviour
+{
+	static int Dodge = Animator.StringToHash("Dodge");
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		MobMotion motion = animator.GetComponent<MobMotion>();
+		if (motion.PendingDodge)
+		{
+			animator.SetTrigger(Dodge);
+			motion.PendingDodge = false;
+		}
+	}
+}
