f09319d4b6864625aa2f430b67afab155865ac05
Tanner Evins <Tanner Evins>
2016-09-14 22:31:51 -0500

    Add StateMachine class and State interface

---
+++ unity/CaulsCastle/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface State
+{
+	void Update(GameObject gameObject);
+}
+
+public class StateMachine
+{
+	State mCurrState;
+
+	public StateMachine(State initialState)
+	{
+		mCurrState = initialState;
+	}
+
+	public void Update(GameObject gameObject)
+	{
+		mCurrState.Update(gameObject);
+	}
+}
