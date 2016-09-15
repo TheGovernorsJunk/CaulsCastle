988e52f7492bac14b7321a0addddbe5f0fa0d3a8
Tanner Evins <Tanner Evins>
2016-09-14 22:32:15 -0500

    Implement entity sensing in AttackOnSight

---
+++ unity/CaulsCastle/Assets/Scripts/MachineStates/AttackOnSight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class AttackOnSight : State
+{
+	static AttackOnSight mSingleton;
+
+	public static State GetInstance()
+	{
+		if (mSingleton != null)
+		{
+			return mSingleton;
+		}
+		mSingleton = new AttackOnSight();
+		return mSingleton;
+	}
+
+	AttackOnSight() {}
+
+	public void Update(GameObject avatar)
+	{
+		LockOn lockOn = avatar.GetComponent<LockOn>();
+		if (lockOn.Target) return;
+
+		SimpleAI props = avatar.GetComponent<SimpleAI>();
+		Vector3 position = avatar.transform.position;
+		Collider2D[] collisions = Physics2D.OverlapCircleAll(position, props.SenseRadius, props.SenseMask);
+		Collider2D playerCollider = collisions.FirstOrDefault(c => c.tag == "Player");
+		if (playerCollider) lockOn.Trigger(playerCollider.transform.position - position);
+	}
+}
